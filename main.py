30c60f423db64850bb38d02445ffc62367f1a692
volvet <volvet@163.com>
2026-06-18 14:33:27 +0800

    simple stragety

---
+++ main.py
@@ -0,0 +1,48 @@
+
+import backtrader as bt
+import yfinance as yf
+import os
+import pandas as pd
+
+
+# basic simple moving average crossover strategy from tutorial
+class SmaCross(bt.Strategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=10,  # period for the fast moving average
+        pslow=30   # period for the slow moving average
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+
+    def next(self):
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
+
+
+def main():
+    print("Yfinance version:", yf.__version__)
+    print("Backtrader version:", bt.__version__)
+    proxy = 'http://127.0.0.1:7897'
+    os.environ['HTTP_PROXY'] = proxy
+    os.environ['HTTPS_PROXY'] = proxy
+    data_df = yf.download('MSFT', start='2020-01-01', end='2022-01-01', multi_level_index=False)
+    print("Data head:\n", data_df.head())
+    data = bt.feeds.PandasData(dataname=data_df)
+    print("Data feed created with lines:", data.lines)
+    cerebro = bt.Cerebro()
+    cerebro.adddata(data)
+    cerebro.addstrategy(SmaCross)
+    cerebro.run()
+    cerebro.plot()
+
+
+if __name__ == "__main__":
+    main()

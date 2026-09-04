4b3cb90906ef7899fec9157473005aaa2563db6a
volvet <volvet@163.com>
2026-09-04 15:28:17 +0800

    Use target network

--- nn/dnn.py
+++ nn/dnn.py
@@ -11,88 +11,95 @@ import random
 from collections import deque
 
 class Agent:
-	def __init__(self, state_size, is_eval=False, model_name=""):
-		self.state_size = state_size # normalized previous days
-		self.action_size = 3 # sit, buy, sell
-		self.memory = deque(maxlen=1000)
-		self.inventory = []
-		self.model_name = model_name
-		self.is_eval = is_eval
+    def __init__(self, state_size, is_eval=False, model_name=""):
+        self.state_size = state_size # normalized previous days
+        self.action_size = 3 # sit, buy, sell
+        self.memory = deque(maxlen=1000)
+        self.inventory = []
+        self.model_name = model_name
+        self.is_eval = is_eval
 
-		self.gamma = 0.95
-		self.epsilon = 1.0
-		self.epsilon_min = 0.01
-		self.epsilon_decay = 0.995
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
 
-		self.model = load_model("models/" + model_name) if is_eval else self._model()
+        self.model = load_model(sys.path[0] + "/models/" + model_name) if is_eval else self._model()
+        self.target_model = keras.models.clone_model(self.model)
+        self.target_model.compile(loss = "mse", optimizer = Adam(learning_rate=0.001))
+        self.target_model.set_weights(self.model.get_weights())
 
-	def _model(self):
-		model = Sequential()
-		model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
-		model.add(Dense(units=32, activation="relu"))
-		model.add(Dense(units=8, activation="relu"))
-		model.add(Dense(self.action_size, activation="linear"))
-		model.compile(loss="mse", optimizer=Adam(learning_rate=0.001))
+    def _model(self):
+        model = Sequential()
+        model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(units=32, activation="relu"))
+        model.add(Dense(units=8, activation="relu"))
+        model.add(Dense(self.action_size, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(learning_rate=0.001))
 
-		return model
+        return model
 
-	def act(self, state):
-		if not self.is_eval and np.random.rand() <= self.epsilon:
-			return random.randrange(self.action_size)
+    def act(self, state):
+        if not self.is_eval and np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
 
-		options = self.model.predict(state)
-		return np.argmax(options[0])
+        options = self.target_model.predict(state)
+        return np.argmax(options[0])
 
-	def expReplay(self, batch_size):
-		mini_batch = []
-		l = len(self.memory)
-		for i in range(l - batch_size + 1, l):
-			mini_batch.append(self.memory[i])
+    def expReplay(self, batch_size):
+        mini_batch = []
+        l = len(self.memory)
+        for i in range(l - batch_size + 1, l):
+            mini_batch.append(self.memory[i])
+        i = 0
+        print(f"Experience replay on {len(mini_batch)} samples")
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
 
-		for state, action, reward, next_state, done in mini_batch:
-			target = reward
-			if not done:
-				target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-
-			target_f = self.model.predict(state)
-			target_f[0][action] = target
-			self.model.fit(state, target_f, epochs=1, verbose='auto')
-
-		if self.epsilon > self.epsilon_min:
-			self.epsilon *= self.epsilon_decay
+            target_f = self.target_model.predict(state)
+            target_f[0][action] = target
+            history = self.model.fit(state, target_f, epochs=1, verbose='auto')
+            if i % 10 == 0:
+                print(f"Iterator {i} loss: {history.history['loss'][0]}")
+            i = i + 1
+        self.target_model.set_weights(self.model.get_weights())
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
 # prints formatted price
 def formatPrice(n):
-	return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
+    return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
 
 # returns the vector containing stock data from a fixed file
 def getStockDataVec(key):
-	vec = []
-	lines = open(sys.path[0] + "/data/" + key + ".csv", "r").read().splitlines()
+    vec = []
+    lines = open(sys.path[0] + "/data/" + key + ".csv", "r").read().splitlines()
 
-	for line in lines[1:]:
-		vec.append(float(line.split(",")[4]))
+    for line in lines[1:]:
+        vec.append(float(line.split(",")[4]))
 
-	return vec
+    return vec
 
 # returns the sigmoid
 def sigmoid(x):
-	return 1 / (1 + math.exp(-x))
+    return 1 / (1 + math.exp(-x))
 
 # returns an an n-day state representation ending at time t
 def getState(data, t, n):
-	d = t - n + 1
-	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
-	res = []
-	for i in range(n - 1):
-		res.append(sigmoid(block[i + 1] - block[i]))
+    d = t - n + 1
+    block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
+    res = []
+    for i in range(n - 1):
+        res.append(sigmoid(block[i + 1] - block[i]))
 
-	return np.array([res])
+    return np.array([res])
 
 
 if len(sys.argv) != 4:
-	print("Usage: python train.py [stock] [window] [episodes]")
-	exit()
+    print("Usage: python train.py [stock] [window] [episodes]")
+    exit()
 
 keras.utils.disable_interactive_logging()
 
@@ -104,40 +111,40 @@ l = len(data) - 1
 batch_size = 32
 
 for e in range(episode_count + 1):
-	print("Episode " + str(e) + "/" + str(episode_count))
-	state = getState(data, 0, window_size + 1)
+    print("Episode " + str(e) + "/" + str(episode_count))
+    state = getState(data, 0, window_size + 1)
 
-	total_profit = 0
-	agent.inventory = []
+    total_profit = 0
+    agent.inventory = []
 
-	for t in range(l):
-		action = agent.act(state)
+    for t in range(l):
+        action = agent.act(state)
 
-		# sit
-		next_state = getState(data, t + 1, window_size + 1)
-		reward = 0
+        # sit
+        next_state = getState(data, t + 1, window_size + 1)
+        reward = 0
 
-		if action == 1: # buy
-			agent.inventory.append(data[t])
-			print ("Buy: " + formatPrice(data[t]))
+        if action == 1: # buy
+            agent.inventory.append(data[t])
+            print ("Buy: " + formatPrice(data[t]))
 
-		elif action == 2 and len(agent.inventory) > 0: # sell
-			bought_price = agent.inventory.pop(0)
-			reward = max(data[t] - bought_price, 0)
-			total_profit += data[t] - bought_price
-			print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+        elif action == 2 and len(agent.inventory) > 0: # sell
+            bought_price = agent.inventory.pop(0)
+            reward = max(data[t] - bought_price, 0)
+            total_profit += data[t] - bought_price
+            print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
-		done = True if t == l - 1 else False
-		agent.memory.append((state, action, reward, next_state, done))
-		state = next_state
+        done = True if t == l - 1 else False
+        agent.memory.append((state, action, reward, next_state, done))
+        state = next_state
 
-		if done:
-			print ("--------------------------------")
-			print ("Total Profit: " + formatPrice(total_profit))
-			print ("--------------------------------")
+        if done:
+            print ("--------------------------------")
+            print ("Total Profit: " + formatPrice(total_profit))
+            print ("--------------------------------")
 
-		if len(agent.memory) > batch_size:
-			agent.expReplay(batch_size)
+        if len(agent.memory) > batch_size:
+            agent.expReplay(batch_size)
 
-	if e % 10 == 0:
-		agent.model.save(sys.path[0]+ "/models/model_ep" + str(e) + '.keras')
+    if e % 10 == 0:
+        agent.model.save(sys.path[0]+ "/models/model_ep" + str(e) + '.keras')

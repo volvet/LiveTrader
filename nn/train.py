064a25c08110f14fd23446b74f2b093d00c734b1
volvet <volvet@163.com>
2026-09-23 14:37:10 +0800

    using gym_trading_env

--- nn/train.py
+++ nn/train.py
@@ -6,6 +6,7 @@ import numpy as np
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+import gym_trading_env
 
 from trade_env import StocksEnv
 from dqn_agent import DQNAgent, DQNConfig
@@ -53,9 +54,54 @@ def train(ticker='AAPL', start_date='2020-01-01', end_date='2021-01-01', epochs 
             next_observation = observation
         agent.save()
 
+# Create your own reward function with the history object
+def reward_function(history):
+    return np.log(history["portfolio_valuation", -1] / history["portfolio_valuation", -2]) #log (p_t / p_t-1 )
+
+def test(ticker='AAPL', start_date='2020-01-01', end_date='2021-01-01'):
+    data = utils.net.download_data(ticker, start_date, end_date)
+    if data is None:
+        return
+    data.rename(columns={'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Volume': 'volume'}, inplace=True)
+    print(data.head)
+    df = data
+    df["feature_close"] = df["close"].pct_change()
+    df["feature_open"] = df["open"]/df["close"]
+    df["feature_high"] = df["high"]/df["close"]
+    df["feature_low"] = df["low"]/df["close"]
+    df["feature_volume"] = df["volume"] / df["volume"].rolling(7*24).max()
+    df.dropna(inplace= True)
+    
+    env = gym.make(
+        "TradingEnv",
+        name = "TradingEnv-v0",
+        df = df,
+        windows= 5,
+        positions = [ -1, -0.5, 0, 0.5, 1, 1.5, 2], # From -1 (=SHORT), to +1 (=LONG)
+        initial_position = 'random', #Initial position
+        trading_fees = 0.01/100, # 0.01% per stock buy / sell
+        borrow_interest_rate= 0.0003/100, #per timestep (= 1h here)
+        reward_function = reward_function,
+        portfolio_initial_value = 1000, # in FIAT (here, USD)
+        max_episode_duration = 'max',
+        disable_env_checker= True
+    )
+    env.add_metric('Position Changes', lambda history : np.sum(np.diff(history['position']) != 0) )
+    env.add_metric('Episode Lenght', lambda history : len(history['position']) )
+
+    done, truncated = False, False
+    observation, info = env.reset()
+    print(info)
+    while not done and not truncated:
+        action = env.action_space.sample()
+        observation, reward, done, truncated, info = env.step(action)
+        print(observation)
+
 if __name__ == "__main__":
     utils.net.setup_proxy(utils.net.PROXY)
-    train()
+    #train()
+    test()
+    
 
 
 
